77a22c6704ef79a9578a2dfe2addb618192912c6
marva00 <145064169+marva00@users.noreply.github.com>
2025-02-14 10:52:37 +0500

    Add files via upload

---
+++ SP23-BCS-140.py
@@ -0,0 +1,374 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello, World!")
+
+
+# In[2]:
+
+
+IF 5 < 2:
+("PYTHON IS A GOOD LANGUAGE")
+
+
+# In[3]:
+
+
+if 5 < 2:
+    ("python is a good language")
+
+
+# In[4]:
+
+
+if 1 < 2:
+    ("python is a good language")
+
+
+# In[7]:
+
+
+if 1 < 2:
+      print ("python is a good language")
+
+
+# In[8]:
+
+
+if 1 < 2:
+       print (python is a good language)
+
+
+# In[9]:
+
+
+x = 5
+
+
+# In[10]:
+
+
+print x
+
+
+# In[11]:
+
+
+x = 10
+print(x)
+
+
+# In[12]:
+
+
+y = "hello world"
+print(y)
+
+
+# In[13]:
+
+
+#hello
+print(x)
+
+
+# In[14]:
+
+
+x = string(3)
+
+
+# In[15]:
+
+
+x = str(3)
+
+
+# In[16]:
+
+
+print(x)
+
+
+# In[17]:
+
+
+x = str(marva)
+
+
+# In[18]:
+
+
+x = "mar"
+x = int(mar)
+
+
+# In[19]:
+
+
+x = str("mar")
+
+
+# In[20]:
+
+
+x = int("mar")
+print (x)
+
+
+# In[21]:
+
+
+x = str("mar")
+print (x)
+
+
+# In[22]:
+
+
+x = 5
+y = "john"
+print type(x)
+print type(y)
+
+
+# In[23]:
+
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+# In[24]:
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+# In[25]:
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+# In[26]:
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+# In[27]:
+
+
+x = 5
+y = "John"
+print(x, y)
+
+
+# In[28]:
+
+
+print(10 + 5)
+
+
+# In[29]:
+
+
+print((6 + 3) - (6 + 3))
+
+
+# In[30]:
+
+
+print(5 + 4 - 7 + 3)
+
+
+# In[31]:
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+
+# In[32]:
+
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+
+# In[33]:
+
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+
+# In[34]:
+
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+
+# In[35]:
+
+
+print (list1)
+
+
+# In[36]:
+
+
+list1 = ["abc", 34, True, 40, "male"]
+
+
+# In[37]:
+
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+
+# In[38]:
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+
+# In[39]:
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+
+# In[40]:
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+
+# In[41]:
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+
+# In[42]:
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+# In[43]:
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+
+# In[44]:
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[] = ["watermelon"]
+print(thislist)1:3
+
+
+# In[45]:
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+
+# In[46]:
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+
+# In[47]:
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+
+# In[48]:
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+
+# In[49]:
+
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+
+# In[50]:
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+
+# In[51]:
+
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+ print(thislist[i])
+
+
+# In[52]:
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+
+# In[ ]:
+
+
+
+
